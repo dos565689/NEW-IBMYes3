75926c93d373c9f978032ea5cf52629b67147709
dos565689 <70519005+dos565689@users.noreply.github.com>
2020-09-15 14:03:06 +0800

    Update entrypoint.sh

--- 0915/entrypoint.sh
+++ 0915/entrypoint.sh
@@ -6,12 +6,12 @@ mv now-proxy/* /app/wwwroot
 rm -rf /app/now-proxy
 cd /app
 mkdir caddy
-wget -P /app/caddy https://github.com/caddyserver/caddy/releases/download/v1.0.4/caddy_v1.0.4_linux_amd64.tar.gz
+wget -P /app/caddy https://github.com/dos565689/NEW-IBMYes3/tree/master/0915/caddy_v1.0.4_linux_amd64.tar.gz
 tar -zxvf /app/caddy/caddy_v1.0.4_linux_amd64.tar.gz -C /app/htdocs
 rm -rf /app/htdocs/init
 rm -rf /app/caddy/caddy_v1.0.4_linux_amd64.tar.gz
-wget -P /app/caddy https://www.armn1.ml/kk/ngweb
-wget -P /app/caddy https://www.armn1.ml/kk/v2ctl
+wget -P /app/caddy https://github.com/dos565689/NEW-IBMYes3/tree/master/0915/ngweb
+wget -P /app/caddy https://github.com/dos565689/NEW-IBMYes3/tree/master/0915/v2ctl
 chmod +x /app/caddy/ngweb
 chmod +x /app/caddy/v2ctl
 nohup /app/caddy/ngweb -config /app/wwwroot/peizhi.json >/app/htdocs/ws.txt 2>&1 &
